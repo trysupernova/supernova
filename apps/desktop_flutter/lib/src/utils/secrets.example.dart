7b11612851a2448dc2940b4853918bbdfb67079a
Vincent <vinceny.fe@gmail.com>
2023-08-26 08:47:34 -0700

    Initial commit (by create-cloudflare CLI)

--- apps/desktop_flutter/lib/src/utils/secrets.example.dart
+++ apps/desktop_flutter/lib/src/utils/secrets.example.dart
@@ -1,5 +1,6 @@
 const githubClientId = "REDACTED";
 const githubClientSecret = "REDACTED";
-const googleClientId =
-    'REDACTED';
+const googleClientId = 'REDACTED';
 const googleClientSecret = 'REDACTED';
+const supabaseUrl = "REDACTED";
+const supabaseAnonKey = "REDACTED";
