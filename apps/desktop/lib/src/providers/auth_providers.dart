7417cfa0dbf8aaebdcd60582db2516927ffb9a96
Vincent Vu <vinceny.fe@gmail.com>
2023-09-08 22:54:25 -0700

    deleted v1 desktop

--- apps/desktop/lib/src/providers/auth_providers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
-final accessTokenProvider = FutureProvider<String?>((ref) {
-  // fetch access_token from secure storage or null
-  const storage = FlutterSecureStorage();
-  final hasAT = storage.read(key: "access_token");
-  return hasAT;
-});
--- apps/desktop/lib/src/utils/secrets.example.dart
+++
@@ -1,6 +0,0 @@
-const githubClientId = "REDACTED";
-const githubClientSecret = "REDACTED";
-const googleClientId = 'REDACTED';
-const googleClientSecret = 'REDACTED';
-const supabaseUrl = "REDACTED";
-const supabaseAnonKey = "REDACTED";
