faeb462704c44a69c64801554678f6ea32ba65ec
snokpok <vinceny.fe@gmail.com>
2023-09-16 14:15:28 -0700

    change next.config depending on build context

--- apps/desktop-v2/next.config.js
+++ apps/desktop-v2/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export",
+    output: process.env.ENV==="desktop" ? "export" : "standalone",
     transpilePackages: ["@supernova/api-client"],
 }
 
